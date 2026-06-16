a7e91f9c1107a62f04c09a70f1e411d59e98dc7d
swarnim <swarnimsaxena01@gmail.com>
2026-06-15 18:08:47 -0700

    adv

---
+++ class 10/advanced/advanceTur.py
@@ -0,0 +1,8 @@
+import turtle
+t = turtle.Turtle()
+for _ in range(4):
+     t.forward(300) 
+     t.right(500)
+     t.backward(400)
+     t.fillcolor('red')
+   
---
+++ class 10/advanced/advanced dice game of woe.py
@@ -0,0 +1,7 @@
+import random
+swarnim=random .randint(1,6)
+mama=random.randint(1,6) 
+if swarnim>=mama:
+    print(f"the winner is swarnim who got the score {swarnim}")
+else:    
+    print(f"the winner is mama who got the score{mama}")
